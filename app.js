43c9a309c50df704df2b537fc7d7508b60cc84d9
szmma <36878461+szmma@users.noreply.github.com>
2019-12-05 13:57:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const CosmosClient = require('@azure/cosmos').CosmosClient
      process.exit(1)
    })
 
- app.get('/', (req, res, next) => taskList.showTasks(req, res).catch(next))
+ app.get('/get', (req, res, next) => taskList.showTasks(req, res).catch(next))
  app.post('/addtask', (req, res, next) => taskList.addTask(req, res).catch(next))
  app.post('/completetask', (req, res, next) =>
    taskList.completeTask(req, res).catch(next)
