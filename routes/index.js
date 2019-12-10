825bd88a3048d97c02ce50815281b3a39bf4d990
szmma <36878461+szmma@users.noreply.github.com>
2019-12-10 21:56:33 +0100

    Update index.js
    API GET-MENU

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/menu', function(req, res, next) {
+});
 
 module.exports = router;
