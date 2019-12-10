7234ee7e38d4518bb0d89160154879d6f0f94b5c
szmma <36878461+szmma@users.noreply.github.com>
2019-12-10 21:51:33 +0100

    Update config.js
    API GET-MENU

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ config.authKey =
   process.env.AUTH_KEY || "OO50Ifk0DmjgRiWJrMhIHdfi5pH2u9PwAVmUtX8liQgUrDQMBwGAato6LG6E4COaelT43ICfhKYmgvQEYxqT4Q==";
 config.databaseId = "ToDoList";
 config.containerId = "Items";
+config.containerId2 = "Menu";
 
 if (config.host.includes("https://localhost:")) {
   console.log("Local environment detected");
@@ -12,7 +13,5 @@ if (config.host.includes("https://localhost:")) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   console.log(`Go to http://localhost:${process.env.PORT || '3000'} to try the sample.`);
 }
-//
 
-//
 module.exports = config;
